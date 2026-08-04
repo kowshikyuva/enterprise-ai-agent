c16fc0b1e50c5e18c72f90444f3ff6daab797462
kowshikyuva <kowshikhv20@gmail.com>
2026-08-04 11:26:57 +0530

    adddeployed frontend url to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://enterprise-ai-agent-git-main-gowda2.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
